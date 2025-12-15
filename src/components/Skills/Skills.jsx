621a12524736154bd1bb2485094aae45b47f2786
developcoding <omidvarisl@gmx.de>
2025-12-15 14:57:24 +0100

    skills wrote 10 percantage

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -11,7 +11,25 @@ export const Skills = () => {
         <span className="skillDesc">I am a skilled and passionate web designer with a strong focus on creating clean, responsive, and user-friendly websites that combine modern design principles with solid technical implementation.</span>
         <div className="skillBars">
             <div className="skillBar">
-                <img src="" alt="skill-image" className="skillBarImg" />
+                <img src={UIDsign} alt="UIDsign" className="skillBarImg" />
+                <div className="skillbarText">
+                    <h2>UI/UX Design</h2>
+                    <p>This is demo text. you can write your own content here</p>
+                </div>
+            </div>
+            <div className="skillBar">
+                <img src={WebDsign} alt="WebDsign" className="skillBarImg" />
+                <div className="skillbarText">
+                    <h2>Website Design</h2>
+                    <p>This is demo text. you can write your own content here</p>
+                </div>
+            </div>
+            <div className="skillBar">
+                <img src={AppDsign} alt="AppDsign" className="skillBarImg" />
+                <div className="skillbarText">
+                    <h2>App Design</h2>
+                    <p>This is demo text. you can write your own content here</p>
+                </div>
             </div>
         </div>
     </section>
